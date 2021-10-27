a8f4e9de904bfa85fc0cf0f66ae690bfc5182d14
Graham Hall <ghall89@me.com>
2021-10-27 02:19:20 +0000

    scroll to section with keypress

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { useEffect } from 'react';
+import { Link, scroller } from 'react-scroll';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -7,6 +8,7 @@ import {
 	faEnvelope
 } from '@fortawesome/free-solid-svg-icons';
 
+// nav links
 const Nav = () => {
 	const links = [
 		{
@@ -26,8 +28,44 @@ const Nav = () => {
 		}
 	];
 
+	// detect keyboard input and scroll to section
+	useEffect(() => {
+		function handleKeyDown(e) {
+			// if '1' or 'a' is pressed, scroll to about section
+			if (e.keyCode === 49 || e.keyCode === 65) {
+				scroller.scrollTo('about', {
+					smooth: true,
+					offset: -140,
+					duration: 500
+				});
+				// if '2' or 'p' is pressed, scroll to portfolio section
+			} else if (e.keyCode === 50 || e.keyCode === 80) {
+				scroller.scrollTo('portfolio', {
+					smooth: true,
+					offset: 0,
+					duration: 500
+				});
+				// if '3' or 'c' is pressed, scroll to contact section
+			} else if (e.keyCode === 51 || e.keyCode === 67) {
+				scroller.scrollTo('contact', {
+					smooth: true,
+					offset: 0,
+					duration: 500
+				});
+			}
+		}
+
+		document.addEventListener('keydown', handleKeyDown);
+
+		// event clean up
+		return function cleanup() {
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+
 	return (
 		<>
+			{/* sidebar for desktop view */}
 			<div className="fixed flex flex-wrap items-center left-0 top-0 h-full w-12 bg-gray-100 text-2xl shadow-xl z-50 invisible md:visible">
 				<div className="flex flex-wrap space-y-1.5">
 					{links.map(link => (
@@ -45,6 +83,7 @@ const Nav = () => {
 					))}
 				</div>
 			</div>
+			{/* bottom bar for mobile view */}
 			<div className="fixed flex flex-wrap justify-around bottom-0 left-0 h-16 w-full bg-gray-100 text-4xl shadow-xl z-50 visible md:invisible">
 				{links.map(link => (
 					<Link
