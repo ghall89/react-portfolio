8253346265bb81da52d60f0ebc041cc5625f769d
Graham Hall <ghall89@me.com>
2021-10-29 02:13:08 +0000

    fixed filename

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,6 +4,6 @@ import Nav from './Nav';
 import Portfolio from './Portfolio';
 import Skills from './Skills';
 import Social from './Social';
-import Testimonials from './Testamonials';
+import Testimonials from './Testimonials';
 
 export { About, Contact, Nav, Portfolio, Skills, Social, Testimonials };
