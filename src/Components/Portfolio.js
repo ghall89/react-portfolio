b8c3f8089ccc755e1c11e811bc2b2a71b8c7443e
Graham Hall <ghall89@me.com>
2022-04-15 16:27:15 +0000

    moved portfolio data out of server

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,8 @@
 import { Slide } from 'react-slideshow-image';
 import { useState, useEffect } from 'react';
 
+import portfolioData from '../data/portfolio.json';
+
 const Portfolio = () => {
 	const fadeProperties = {
 		duration: 5000,
@@ -10,71 +12,49 @@ const Portfolio = () => {
 		arrows: false
 	};
 
-	const [portfolio, setPortfolio] = useState();
+	const [portfolio, setPortfolio] = useState(portfolioData);
 
-	useEffect(async () => {
-		const rsp = await fetch('https://ghalldev-backend.herokuapp.com/');
-		if (rsp.ok) {
-			const json = await rsp.json();
-			setPortfolio(json.records);
-		} else {
-			alert('HTTP-Error: ' + rsp.status);
-		}
-	}, []);
-
-	if (!portfolio) {
-		return (
-			<div className="flex justify-center items-center">
-				<div className="w-52 h-14">
-					<div className="animate-bounce text-blue-400 text-center text-2xl mt-2.5">
-						Loading...
-					</div>
-				</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className="slide-container">
-				<Slide {...fadeProperties}>
-					{portfolio.map(data => (
-						<div
-							className="md:flex bg-gradient-to-br from-blue-500 to-purple-400 shadow-inner"
-							key={Math.random()}
-						>
-							<div className="flex-1">
-								<img src={data.img} alt={data.title} />
+	return (
+		<div className="slide-container">
+			<Slide {...fadeProperties}>
+				{portfolio.map(data => (
+					<div
+						className="md:flex bg-gradient-to-br from-blue-500 to-purple-400 shadow-inner"
+						key={Math.random()}
+					>
+						<div className="flex-1">
+							<img src={data.img} alt={data.title} />
+						</div>
+						<div className="flex-1 flex text-white relative h-64 md:h-auto pb-16">
+							<div className="text-center self-center w-full ">
+								<h4 className="text-2xl font-bold	m-4">{data.title}</h4>
+								<p className="m-4">{data.description}</p>
+								<p className="m-4">Tools: {data.tags.join(', ')}</p>
 							</div>
-							<div className="flex-1 flex text-white relative h-64 md:h-auto pb-16">
-								<div className="text-center self-center w-full ">
-									<h4 className="text-2xl font-bold	m-4">{data.title}</h4>
-									<p className="m-4">{data.description}</p>
-									<p className="m-4">Tools: {data.tags.join(', ')}</p>
-								</div>
-								<div className="absolute bottom-5 flex px-3 space-x-3 w-full text-center">
-									<a
-										href={data.url}
-										target="_blank"
-										rel="noreferrer"
-										className="p-2 rounded-md flex-grow text-blue-500 bg-white shadow flex-1 transition-transform transform hover:-translate-y-0.5 hover:shadow-md"
-									>
-										<span className="ml-3">Visit Page</span>
-									</a>
-									<a
-										href={data.github}
-										target="_blank"
-										rel="noreferrer"
-										className="p-2 rounded-md flex-grow text-blue-500 bg-white shadow flex-1 transition-transform transform hover:-translate-y-0.5 hover:shadow-md"
-									>
-										<span className="ml-3">GitHub</span>
-									</a>
-								</div>
+							<div className="absolute bottom-5 flex px-3 space-x-3 w-full text-center">
+								<a
+									href={data.url}
+									target="_blank"
+									rel="noreferrer"
+									className="p-2 rounded-md flex-grow text-blue-500 bg-white shadow flex-1 transition-transform transform hover:-translate-y-0.5 hover:shadow-md"
+								>
+									<span className="ml-3">Visit Page</span>
+								</a>
+								<a
+									href={data.github}
+									target="_blank"
+									rel="noreferrer"
+									className="p-2 rounded-md flex-grow text-blue-500 bg-white shadow flex-1 transition-transform transform hover:-translate-y-0.5 hover:shadow-md"
+								>
+									<span className="ml-3">GitHub</span>
+								</a>
 							</div>
 						</div>
-					))}
-				</Slide>
-			</div>
-		);
-	}
+					</div>
+				))}
+			</Slide>
+		</div>
+	);
 };
 
 export default Portfolio;
